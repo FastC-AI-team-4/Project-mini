7042a09a951307e13e52b2b46eb6b127949f33c1
gacenti <wjsrktks@gmail.com>
2023-02-06 15:48:38 +0900

    feat: Done fizzbuzz
    Print fizzbuzz, if i is multiple of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 for i in range(1,50):
-    if i%3 ==0:
+    if i%15==0:
+        print(f'{i}: fizzbuzz')
+    elif i%3 ==0:
         print(f'{i}: fizz')
     elif i%5==0:
         print(f'{i}: buzz')

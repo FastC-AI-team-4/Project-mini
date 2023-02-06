beb1ec2d29c9cf37b9e84f6f325b6ce7d48f2ae7
keejuneman <rlwns9603@naver.com>
2023-02-06 14:51:28 +0900

    feat: Add next_player funcition, Error Correction

--- Pig-dice.py
+++ Pig-dice.py
@@ -8,28 +8,39 @@ def roll_dice():
 
 
 # 플레이어 구현
-score = {
-    "player1": 0,
-    "player2": 0
-}
-current_player = "player1"
-value = score.values()
-# 목표점수
-max_score = 100
-# roll_dice를 통한 모은 임시 점수
-temp_score = 0
-while value < max_score:
-    roll = roll_dice()
-    # 주사위가 1이 나온경우 모은 스코어
-    if roll == 1:
-        temp_score = 0
-        # 다음사람으로 넘어가는 기능 구현해야함
-    # 주사위가 1외의 숫자인 경우
-    else:
-        temp_score += roll
-        ans = input(f"{current_player} rolled {roll}"
-                    f"Current Score {temp_score}"
-                    "Do you want to roll again? Y/N")
-        if ans.lower() != "y":
-            score[current_player] += temp_score
-            # 다음사람으로 넘어가는 기능 구현해야함
+def pig_dice(max_score):
+    score = {
+        "player1": 0,
+        "player2": 0
+    }
+    current_player = "player1"
+    value = score.values()
+    # roll_dice를 통한 모은 임시 점수
+    temp_score = 0
+    while max(value) < max_score:
+        roll = roll_dice()
+        # 주사위가 1이 나온경우 모은 스코어
+        if roll == 1:
+            print("rolled 1")
+            temp_score = 0
+            # 다음사람으로 넘어가는 기능
+            current_player = next_player(current_player)
+        # 주사위가 1외의 숫자인 경우
+        else:
+            temp_score += roll
+            print(f"{current_player} rolled : {roll}")
+            print(f"Total Score : {score[current_player]}")
+            print(f"Current Score : {temp_score}")
+            ans = input("Do you want to roll again? Y/N")
+            if ans.lower() == "n":
+                score[current_player] += temp_score
+                temp_score = 0
+                # 다음사람으로 넘어가는 기능
+                current_player = next_player(current_player)
+
+
+def next_player(current_player):
+    return "player2" if current_player == "player1" else "player1"
+
+
+pig_dice(100)
